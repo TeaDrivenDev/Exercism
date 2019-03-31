e64bbed6223ca094d69b989be997beb350f840b2
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-03-31 20:14:37 +0200

    (F#) Update Leap Year exercise

---
+++ fsharp/leap/Leap.fs
@@ -0,0 +1,10 @@
+﻿module Leap
+
+let inline (<&&>) f g x = f x && g x
+let inline (<||>) f g x = f x || g x
+
+let isDivisibleBy divisor dividend = dividend % divisor = 0
+
+let leapYear =
+    isDivisibleBy 4
+    <&&> (not << isDivisibleBy 100 <||> isDivisibleBy 400)
