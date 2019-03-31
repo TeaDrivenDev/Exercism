875d04f72925f80db67e1e577b00f6cc60ff5620
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-03-31 17:17:44 +0200

    (F#) Update Pig Latin exercise

---
+++ fsharp/pig-latin/PigLatin.fs
@@ -0,0 +1,26 @@
+module PigLatin
+
+let trueVowels = [ "a"; "e"; "i"; "o"; "u" ]
+let vowels = trueVowels @ [ "yt"; "xr" ]
+
+let trueConsonants =
+    Set.ofList ([ 'a' .. 'z' ] |> List.map string)
+    - Set.ofList trueVowels
+    |> Set.toList
+
+let specialConsonants = [ "ch"; "qu"; "sch"; "thr"; "th"; "rh" ]
+let xquConsonants = trueConsonants |> List.map (fun c -> c + "qu")
+let consonants = specialConsonants @ xquConsonants @ trueConsonants
+
+let (|StartsWith|_|) (values : string list) (word : string) =
+    values |> List.tryFind word.StartsWith
+    |> Option.map (fun c -> c, word.[c.Length..])
+
+let translate (phrase : string) =
+    phrase.Split [| ' ' |]
+    |> Array.map (fun word ->
+        match word with
+        | StartsWith vowels _ -> word + "ay"
+        | StartsWith consonants (c, rest) -> rest + c + "ay"
+        | _ -> failwith (sprintf "Invalid start of word: %c" word.[0]))
+    |> String.concat " "
