244436f08e4187c051b08b93063e1ca20504f1c7
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-04-01 12:47:12 +0200

    (F#) Update Sum of Multiples exercise

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -3,8 +3,6 @@ module SumOfMultiples
 let isMultiple x y = y <> 0 && x % y = 0
 
 let sum numbers limit =
-    (0, [ 1 .. limit - 1 ])
-    ||> List.fold (fun acc current ->
-        if numbers |> List.exists (isMultiple current)
-        then acc + current
-        else acc)
+    [ 1 .. limit - 1 ]
+    |> List.filter (fun current -> numbers |> List.exists (isMultiple current))
+    |> List.sum
