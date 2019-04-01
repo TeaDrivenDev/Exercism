982bc263a3f9bc5dd551a6803db56583c0a43166
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-04-01 21:17:53 +0200

    Stash commit

---
+++ fsharp/all-your-base/AllYourBase.fs
@@ -0,0 +1,16 @@
+﻿module AllYourBase
+
+open System
+
+let rebase digits inputBase outputBase =
+    let value =
+        digits
+        |> List.rev
+        |> List.mapi (fun index digit ->
+            float inputBase ** float index * float digit)
+        |> List.sum
+
+    let xx =
+        [ Math.Log(value, float outputBase) |> Math.Floor |> int .. 0 ]
+
+Math.Log(16., 10.)
