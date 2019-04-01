2b9ed1d2b3df437710ac21ff928fcc1a3cecb59c
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-04-01 20:41:37 +0200

    (F#) Update Atbash Cipher exercise

---
+++ fsharp/atbash-cipher/AtbashCipher.fs
@@ -0,0 +1,20 @@
+﻿module AtbashCipher
+
+open System.Text.RegularExpressions
+
+let cipher =
+    let atoz, digits = ['a'..'z'], ['0'..'9']
+
+    (atoz @ digits, List.rev atoz @ digits)
+    ||> List.zip
+    |> Map.ofList
+
+let encode (words : string) =
+    Regex.Replace(words.ToLower(), @"[^\d\w]", "")
+    |> Seq.map (fun c -> cipher.[c])
+    |> Seq.chunkBySize 5
+    |> Seq.map System.String
+    |> String.concat " "
+
+let decode (code : string) =
+    (encode code).Replace(" ", "")
