cc23628f7d5b8903ff5bae8a3618fa5d0824d42f
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-04-05 02:51:11 +0200

    (F#) Update Beer Song exercise

--- fsharp/beer-song/BeerSong.fs
+++ fsharp/beer-song/BeerSong.fs
@@ -5,16 +5,16 @@ open System
 let bottles count =
     if count = 1
     then "1 bottle"
-    else (if count = 0
-          then "no more"
-          else string count
-          |> sprintf "%s bottles")
+    else
+        match count with
+        | 0 -> "no more"
+        | _ -> string count
+        |> sprintf "%s bottles"
 
 let capitalize (s : string) =
     if String.IsNullOrWhiteSpace s
     then s
-    else (Char.ToUpper s.[0] |> string)
-         + (if s.Length > 1 then s.Substring 1 else "")
+    else (Char.ToUpper s.[0] |> string) + s.Substring 1
 
 let defaultFirst count =
     let bottles = bottles count
@@ -26,13 +26,11 @@ let defaultSecond count =
 
     sprintf "Take %s down and pass it around, %s of beer on the wall." one (bottles count)
 
-let oneFirst = "1 bottle of beer on the wall, 1 bottle of beer."
 let noneSecond = "Go to the store and buy some more, 99 bottles of beer on the wall."
 
 let verse current =
     match current with
     | 0 -> [ defaultFirst 0; noneSecond ]
-    | 1 -> [ oneFirst; defaultSecond 0 ]
     | _ -> [ defaultFirst current; defaultSecond (current - 1) ]
 
 let recite start count =
