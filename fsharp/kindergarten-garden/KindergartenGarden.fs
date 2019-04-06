2073c9da8a945141429b3adfee648fb0432bfa5b
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-04-06 15:20:06 +0200

    (F#) Solve Kindergarten Garden exercise

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,28 @@
+﻿module KindergartenGarden
+
+type Plant =
+    | Clover
+    | Grass
+    | Radishes
+    | Violets
+
+let plants (diagram : string) (student : string) =
+    let garden =
+        let [| row1; row2 |] =
+            diagram.Split([| '\n' |])
+            |> Array.map (Seq.chunkBySize 2)
+
+        (row1, row2)
+        ||> Seq.zip
+        |> Seq.map (fun (first, second) ->
+            Array.append first second
+            |> Array.toList
+            |> List.map (function
+                | 'C' -> Clover
+                | 'G' -> Grass
+                | 'R' -> Radishes
+                | 'V' -> Violets
+                | _ -> failwith "Unknown plant"))
+        |> Seq.toList
+
+    garden |> List.item (int student.[0] - int 'A')
