72d5f5b91ebf19b91a24fc25278d7b8aa619fb51
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-04-06 14:09:54 +0200

    (F#) Solve Queen Attack exercise

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,11 @@
+﻿module QueenAttack
+
+let create (row : int, column : int) =
+    row >= 0 && column >= 0 && row < 8 && column < 8
+
+let canAttack ((row1, column1) as queen1) ((row2, column2) as queen2) =
+    create queen1 && create queen2
+    && (row1 = row2
+        || column1 = column2
+        || row1 + column1 = row2 + column2
+        || row1 - column1 = row2 - column2)
