8da8d4e32cf7f9b631955b3a882aea260fa5ab7d
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-06-01 22:09:45 +0200

    (F#) Solve Nth Prime exercise

---
+++ fsharp/nth-prime/NthPrime.fs
@@ -0,0 +1,26 @@
+﻿module NthPrime
+
+let isPrime existingPrimes n =
+    existingPrimes
+    |> List.filter (fun x -> x <= (n |> float |> sqrt |> int))
+    |> List.exists (fun x -> n % x = 0)
+    |> not
+
+let rec findPrimes targetCount existingPrimes next =
+    if List.length existingPrimes = targetCount
+    then existingPrimes
+    else
+        let existingPrimes =
+            if next |> isPrime existingPrimes
+            then existingPrimes @ [ next ]
+            else existingPrimes
+
+        findPrimes targetCount existingPrimes (next + 1)
+
+let prime nth : int option =
+    if nth = 0
+    then None
+    else
+        findPrimes nth [ 2 ] 3
+        |> List.last
+        |> Some
