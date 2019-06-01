f4660d0ffd601ed89ba838ed530d258908f9e20b
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-06-01 22:55:21 +0200

    (F#) Improve performance in Nth Prime exercise

--- fsharp/nth-prime/NthPrime.fs
+++ fsharp/nth-prime/NthPrime.fs
@@ -1,26 +1,16 @@
 ﻿module NthPrime
 
-let isPrime existingPrimes n =
-    existingPrimes
-    |> List.filter (fun x -> x <= (n |> float |> sqrt |> int))
-    |> List.exists (fun x -> n % x = 0)
+let isPrime n =
+    seq { 2 .. n |> float |> sqrt |> int }
+    |> Seq.exists (fun x -> n % x = 0)
     |> not
-
-let rec findPrimes targetCount existingPrimes next =
-    if List.length existingPrimes = targetCount
-    then existingPrimes
-    else
-        let existingPrimes =
-            if next |> isPrime existingPrimes
-            then existingPrimes @ [ next ]
-            else existingPrimes
-
-        findPrimes targetCount existingPrimes (next + 1)
 
 let prime nth : int option =
     if nth = 0
     then None
     else
-        findPrimes nth [ 2 ] 3
-        |> List.last
+        Seq.initInfinite id
+        |> Seq.skip 1
+        |> Seq.choose (fun i -> if isPrime i then Some i else None)
+        |> Seq.item nth
         |> Some
