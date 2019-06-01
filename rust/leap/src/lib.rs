632a16f17fa3ec30666ddc59777bfe9444ea2f2c
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-06-01 04:39:05 +0200

    (Rust) Solve Leap exercise

---
+++ rust/leap/src/lib.rs
@@ -0,0 +1,8 @@
+fn is_divisible_by(year: u64, divisor: u64) -> bool {
+    year % divisor == 0
+}
+
+pub fn is_leap_year(year: u64) -> bool {
+    is_divisible_by(year, 4) && !is_divisible_by(year, 100)
+    || is_divisible_by(year, 400)
+}
