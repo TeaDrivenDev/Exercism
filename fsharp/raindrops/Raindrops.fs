7a7755b3a064bc56cb1f737617827513b75daf68
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-06-02 02:32:59 +0200

    (F#) Rework Raindrops exercise

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -1,35 +1,21 @@
 module Raindrops
 
-let findPrimeFactor n =
-    [ 2 .. n/2 ] |> List.tryFind ((%) n >> (=) 0)
+let sounds =
+    [
+        3, "Pling"
+        5, "Plang"
+        7, "Plong"
+    ]
+    |> Map.ofList
 
-let (|Low|NoPrimeFactors|PrimeFactor|) n =
-    if n <= 2
-    then Low
-    else
-        match findPrimeFactor n with
-        | Some f -> PrimeFactor (f, n / f)
-        | None -> NoPrimeFactors
-
-let rec primeFactors n =
-    match n with
-    | Low | NoPrimeFactors -> [ n ]
-    | PrimeFactor (f, remainder) -> f :: primeFactors remainder
-
-let toPling factors =
-    let set = set factors
-
-    [3, "Pling"; 5, "Plang"; 7, "Plong"]
-    |> List.filter (fst >> set.Contains)
-    |> function
-        | [] -> None
-        | factors ->
-            factors
-            |> List.map snd
-            |> String.concat ""
-            |> Some
+let sound number =
+    sounds |> Map.tryFind number |> Option.defaultValue ""
 
 let convert number =
-    primeFactors number
-    |> toPling
-    |> Option.defaultWith (fun () -> string number)
+    [ 1 .. number ]
+    |> List.choose (fun x ->
+        if number % x = 0 then sound x |> Some else None)
+    |> String.concat ""
+    |> function
+        | "" -> string number
+        | s -> s
