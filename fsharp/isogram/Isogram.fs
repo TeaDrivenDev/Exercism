320f464ffbe3c58a293316deaf5650385287d662
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-06-04 03:14:36 +0200

    (F#) Solve Isogram exercise

---
+++ fsharp/isogram/Isogram.fs
@@ -0,0 +1,8 @@
+﻿module Isogram
+
+let isIsogram (str : string) =
+    str.ToLower()
+    |> Seq.filter System.Char.IsLetter
+    |> Seq.groupBy id
+    |> Seq.exists (fun (_, letters) -> Seq.length letters > 1)
+    |> not
