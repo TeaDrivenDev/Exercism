944e40814cdd470772c26e282eef8df286229631
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-06-04 20:52:52 +0200

    (Rust) Update Reverse String exercise

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -2,8 +2,6 @@ use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
     input.graphemes(true)
-        .collect::<Vec<_>>()
-        .into_iter()
         .rev()
         .collect::<String>()
 }
