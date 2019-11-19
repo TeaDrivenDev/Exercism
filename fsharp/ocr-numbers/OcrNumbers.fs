fa56bc8eb11f539a62523d32f188b22675963ba0
TeaDrivenDev <TeaDrivenDev@gmail.com>
2019-11-18 20:30:09 -0400

    (F#) Slightly simplify OCR Numbers

--- fsharp/ocr-numbers/OcrNumbers.fs
+++ fsharp/ocr-numbers/OcrNumbers.fs
@@ -5,48 +5,48 @@ let numbers =
         [ " _ "
           "| |"
           "|_|"
-          "   " ], 0
+          "   " ]
         [ "   "
           "  |"
           "  |"
-          "   " ], 1
+          "   " ]
         [ " _ "
           " _|"
           "|_ "
-          "   " ], 2
+          "   " ]
         [ " _ "
           " _|"
           " _|"
-          "   " ], 3
+          "   " ]
         [ "   "
           "|_|"
           "  |"
-          "   " ], 4
+          "   " ]
         [ " _ "
           "|_ "
           " _|"
-          "   " ], 5
+          "   " ]
         [ " _ "
           "|_ "
           "|_|"
-          "   " ], 6
+          "   " ]
         [ " _ "
           "  |"
           "  |"
-          "   " ], 7
+          "   " ]
         [ " _ "
           "|_|"
           "|_|"
-          "   " ], 8
+          "   " ]
         [ " _ "
           "|_|"
           " _|"
-          "   " ], 9
+          "   " ]
     ]
-    |> Map.ofList
 
 let recognizeDigit input =
-    numbers.TryFind input
+    numbers
+    |> List.tryFindIndex ((=) input)
     |> Option.map string
     |> Option.defaultValue "?"
 
